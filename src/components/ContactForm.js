ee1aa5d3285cb4424192413fb0ffd82b5f90c4fc
Aditya Kamble <adityakamble6101@gmail.com>
2021-07-23 09:15:06 +0530

    contact-form is a sperate component now.

---
+++ src/components/ContactForm.js
@@ -0,0 +1,28 @@
+import "./scss/ContactForm.scss";
+
+const ContactForm = () => {
+  return (
+    <div className="contact-form">
+      <h3>Leave Your Message Here</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="name">Name:</label>
+          <input id="name" maxLength="20" type="text" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email:</label>
+          <input id="email" type="email" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="message">Your Message:</label>
+          <textarea name="message" id="message" cols="10" rows="5"></textarea>
+        </div>
+        <button className="submit-btn" type="submit">
+          Send Message <i class="fas fa-paper-plane"></i>
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,5 +1,5 @@
 import "./scss/ContactSection.scss";
-
+import ContactFrom from "./ContactForm";
 const ContactSection = () => {
   return (
     <section className="contact-section">
@@ -43,31 +43,7 @@ const ContactSection = () => {
             </ul>
           </div>
         </div>
-        <div className="contact-form">
-          <h3>Leave Your Message Here</h3>
-          <form>
-            <div className="form-control">
-              <label htmlFor="name">Name:</label>
-              <input id="name" maxLength="20" type="text" />
-            </div>
-            <div className="form-control">
-              <label htmlFor="email">Email:</label>
-              <input id="email" type="email" />
-            </div>
-            <div className="form-control">
-              <label htmlFor="message">Your Message:</label>
-              <textarea
-                name="message"
-                id="message"
-                cols="10"
-                rows="5"
-              ></textarea>
-            </div>
-            <button className="submit-btn" type="submit">
-              Send Message <i class="fas fa-paper-plane"></i>
-            </button>
-          </form>
-        </div>
+        <ContactFrom />
       </div>
     </section>
   );
