d9870e1e66a1a7079bd0121b5affa4f2610b7734
Aditya Kamble <adityakamble6101@gmail.com>
2021-09-28 12:30:34 +0530

    environment variables added

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,24 +3,22 @@ import emailjs from "emailjs-com";
 
 const ContactForm = () => {
   const sendEmail = (e) => {
+    const serviceId = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+    const templateId = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+    const userId = "" + process.env.REACT_APP_EMAIL_JS_USER_ID;
+
     e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_pnt90rj",
-        "my-portfolio-template",
-        e.target,
-        "user_y2pnmU5DCdz2ix4rLeh5J"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          alert("Message Sent");
-        },
-        (error) => {
-          console.log(error.text);
-          alert("Something Went wrong. Please try again later");
-        }
-      );
+
+    emailjs.sendForm(serviceId, templateId, e.target, userId).then(
+      (result) => {
+        console.log(result.text);
+        alert("Message Sent");
+      },
+      (error) => {
+        console.log(error.text);
+        alert("Something Went wrong. Please try again later");
+      }
+    );
     e.target.reset();
   };
 
@@ -30,15 +28,20 @@ const ContactForm = () => {
       <form onSubmit={sendEmail}>
         <div className="form-control">
           <label htmlFor="name">Name:</label>
-          <input id="name" maxLength="20" type="text" name="name" />
+          <input id="name" maxLength="20" type="text" name="sender_name" />
         </div>
         <div className="form-control">
           <label htmlFor="email">Email:</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="sender_email" />
         </div>
         <div className="form-control">
           <label htmlFor="message">Your Message:</label>
-          <textarea id="message" cols="10" rows="5" name="message"></textarea>
+          <textarea
+            id="message"
+            cols="10"
+            rows="5"
+            name="sender_message"
+          ></textarea>
         </div>
         <button className="submit-btn" type="submit">
           Send Message <i className="fas fa-paper-plane"></i>
